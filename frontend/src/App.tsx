1bbd4bc8e0db3078992d0b9a6762cb7a273af710
System Administrator <etomer9@gmail.com>
2024-10-05 14:06:03 +0300

    mail change 25

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Navbar from './components/Navbar';
